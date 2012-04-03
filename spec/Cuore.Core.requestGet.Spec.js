6466bbdde001c6359e9365304ca5cf54102fbfe0
XaV1Uzz <xaviuzz@gmail.com>
2012-04-03 20:47:18 +0200

    Some simplification in test, better describes, by Xavi

--- spec/Cuore.Core.requestGet.Spec.js
+++ spec/Cuore.Core.requestGet.Spec.js
@@ -1,102 +1,89 @@
 describe("Core request Get", function() {
-    
-    var URL = 'aUrl';
-   
-   it("callback called with json formatted response", function() {
-        var responseObject = {"id":123,"title":"Hollywood - Part 2"};
-        var responseText = JSON.stringify(responseObject);
-        
-        var server = sinon.fakeServer.create();
-        server.respondWith("GET", URL,
-          [200, {"Content-Type": "application/json"}, responseText]);
-        
-        var callback = sinon.spy();
-        CUORE.Core.requestGet(URL, 'someInputData', callback);
-        
-        server.respond(); 
-        
-        expect(callback.calledWith(responseObject)).toBeTruthy();
-    
-        server.restore();
-    });
-    
-    
-    it('should send the data in the url', function(){
-        var xhr = sinon.useFakeXMLHttpRequest();
-        var requests = [];
-        
-        xhr.onCreate = function (xhr) {
-            requests.push(xhr);
-        };
-      
-        CUORE.Core.createXHR = function(){
-            return xhr;
-        };
-  
-        var aJsonInputData = {'key': 'value', 'anotherkey' : 'another_value'};
-        
-        var callback = sinon.spy();
-        CUORE.Core.requestGet(URL, aJsonInputData, callback);
-        
-        var usedXhr = requests[1];
-        var sentUrl = usedXhr.url;
-       
-        var expectedSentUrl = URL + '?key=value&anotherkey=another_value';
-        expect(sentUrl).toBe(expectedSentUrl);
-        
-        xhr.restore();
-    });
 
+    var URL = 'aUrl';
+
+    it("callback called with json formatted response", function() {
+        var responseObject = {
+            "id": 123,
+            "title": "Hollywood - Part 2"
+        };
+        var responseText = JSON.stringify(responseObject);
+        var server = sinon.fakeServer.create();
+        server.respondWith("GET", URL, [200,
+        {
+            "Content-Type": "application/json"
+        },
+        responseText]);
+        var callback = sinon.spy();
+
+        CUORE.Core.requestGet(URL, 'someInputData', callback);
+        server.respond();
+
+        expect(callback.calledWith(responseObject)).toBeTruthy();
+    });
 
     describe("when params are used in the url", function() {
-    
         var xhr;
         var requests;
 
-        beforeEach(function(){
+        beforeEach(function() {
             xhr = sinon.useFakeXMLHttpRequest();
             requests = [];
-            
-            xhr.onCreate = function (xhr) {
+
+            xhr.onCreate = function(xhr) {
                 requests.push(xhr);
             };
-          
-            CUORE.Core.createXHR = function(){
+
+            CUORE.Core.createXHR = function() {
                 return xhr;
             };
         });
 
-        afterEach(function(){
+        afterEach(function() {
             xhr.restore();
         });
 
-        var lastSentUrl = function(){
-            var usedXhr = requests[1];
-            var sentUrl = usedXhr.url;
-           return sentUrl;
-        };
+        var lastSentUrl = function() {
+                var usedXhr = requests[1];
+                var sentUrl = usedXhr.url;
+                return sentUrl;
+            };
 
-        it('parses empty spaces', function(){
-            var aJsonInputData = {'key': 'a value'};
-            
+        it('should send the data in the url', function() {
+            var aJsonInputData = {
+                'key': 'value',
+                'anotherkey': 'another_value'
+            };
+            var expectedSentUrl = URL + '?key=value&anotherkey=another_value';
             var callback = sinon.spy();
-            CUORE.Core.requestGet(URL, aJsonInputData, callback);    
-              
-            var sentUrl = lastSentUrl();
-            var expectedSentUrl = URL + '?key=a%20value';
-            expect(sentUrl).toBe(expectedSentUrl);
+            
+            CUORE.Core.requestGet(URL, aJsonInputData, callback);
+
+            expect(lastSentUrl()).toBe(expectedSentUrl);
         });
 
-        it('scapes non url characters', function(){
-           
-            var aJsonInputData = {'key': 'value&anothervalue'};
-            
+        it('parses empty spaces', function() {
+            var aJsonInputData = {
+                'key': 'a value'
+            };
+            var expectedSentUrl = URL + '?key=a%20value';
             var callback = sinon.spy();
+            
             CUORE.Core.requestGet(URL, aJsonInputData, callback);
             
-            var sentUrl = lastSentUrl();
+            expect(lastSentUrl()).toBe(expectedSentUrl);
+        });
+
+        it('scapes non url characters', function() {
+            var aJsonInputData = {
+                'key': 'value&anothervalue'
+            };
             var expectedSentUrl = URL + '?key=value%26anothervalue';
-            expect(sentUrl).toBe(expectedSentUrl);
+            var callback = sinon.spy();
+            
+            CUORE.Core.requestGet(URL, aJsonInputData, callback);
+            
+            expect(lastSentUrl()).toBe(expectedSentUrl);
         });
 
     });
