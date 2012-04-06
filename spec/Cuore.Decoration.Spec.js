d6f26d7f59d9568dcb7b5fff8195bcdbc6116b04
XaV1Uzz <xaviuzz@gmail.com>
2012-04-06 14:26:12 +0200

    A new matcher brought to you by XaV1Uzz

--- spec/Cuore.Decoration.Spec.js
+++ spec/Cuore.Decoration.Spec.js
@@ -1,14 +1,21 @@
 describe("Decoration", function() {
-    
+
+    beforeEach(function() {
+        this.addMatchers({
+            toBeInstanceOf: CUORE.Matchers.toBeInstanceOf,
+            toBeFunction: CUORE.Matchers.toBeFunction
+        });
+    });
+
     it("is a Decoration", function() {
-        var aDecoration=new CUORE.Decoration();
-        expect(aDecoration instanceof CUORE.Decoration).toBeTruthy();
+        var aDecoration = new CUORE.Decoration();
+        expect(aDecoration).toBeInstanceOf(CUORE.Decoration);
     });
 
     it("has a postPaint function ", function() {
-        var aDecoration=new CUORE.Decoration();
-        expect(typeof aDecoration.postPaint === 'function').toBeTruthy();
-        expect (aDecoration.postPaint.length).toEqual(1);
+        var aDecoration = new CUORE.Decoration();
+        expect(aDecoration.postPaint).toBeFunction();
+        expect(aDecoration.postPaint.length).toEqual(1);
     });
 
 });
