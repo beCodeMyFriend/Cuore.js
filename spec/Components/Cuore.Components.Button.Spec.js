7569c22b259ce583367c068edcf1a8b881c32241
Miguel Angel Fernandez <elmendalerenda@gmail.com>
2012-04-20 12:54:47 +0200

    removed getUniqueID from buttonSpec

--- spec/Components/Cuore.Components.Button.Spec.js
+++ spec/Components/Cuore.Components.Button.Spec.js
@@ -1,53 +1,48 @@
-describe("Button", function () {
-	
+describe("Button", function() {
+
     var xhr;
 
-    beforeEach(function(){
+    beforeEach(function() {
         xhr = sinon.useFakeXMLHttpRequest();
         var requests = [];
-        
-        xhr.onCreate = function (xhr) {
+
+        xhr.onCreate = function(xhr) {
             requests.push(xhr);
         };
-      
-        CUORE.Core.createXHR = function(){
+
+        CUORE.Core.createXHR = function() {
             return xhr;
         };
     });
 
-    afterEach(function(){
+    afterEach(function() {
         var container = document.getElementById('xhtmlToTest');
         container.innerHTML = '';
 
         xhr.restore();
     });
-   
-    it("inherits Component", function () {
+
+    it("inherits Component", function() {
         var aButton = new CUORE.Components.Button();
 
         expect(aButton instanceof CUORE.Components.Button).toBeTruthy();
         expect(aButton instanceof CUORE.Component).toBeTruthy();
     });
 
-    it("sets I18NKey in construct", function () {
+    it("sets I18NKey in construct", function() {
         var aI18NKey = "CanonicalKey";
         var aButton = new CUORE.Components.Button("buttonName", aI18NKey);
         expect(aButton.getButtonText()).toEqual(aI18NKey);
     });
 
-    it("default text is 'Click!!'", function () {
+    it("default text is 'Click!!'", function() {
         var container = createTestContainer();
         var aButton = new CUORE.Components.Button();
-        aButton.setContainer(container.id);
+
         expect(aButton.getButtonText()).toEqual("CLICK!");
-
-        aButton.draw();
-
-        var DOMButton = document.getElementById(aButton.getUniqueID());
-        expect(DOMButton.innerHTML).toMatch("CLICK!");
     });
 
-    it("default name is 'aButton' and service is defaulted to 'BUTTON'", function () {
+    it("default name is 'aButton' and service is defaulted to 'BUTTON'", function() {
         var aButton = new CUORE.Components.Button();
         var aDirectory = CUORE.Mocks.Directory();
         aButton.setDirectory(aDirectory);
@@ -57,64 +52,97 @@ describe("Button", function () {
         expect(aDirectory.execute).toHaveBeenCalledWith("BUTTON", 'aButton', null);
     });
 
-    it("generates an anchor in DOM with a span for css replacement when it is drawn", function () {
-        var container = createTestContainer();
 
-        var buttonName = "buttonName";
-        var aButton = new CUORE.Components.Button(buttonName, "CanonicalKey");
-        aButton.setContainer(container.id);
+    describe('renderer', function() {
 
-        aButton.draw();
+        it("generates an anchor in DOM with a span for css replacement when it is drawn", function() {
+            var container = createTestContainer();
 
-        var buttonText = "buttonText";
-        aButton.setText( "CanonicalKey", buttonText);
+            var buttonText = "buttonText";
 
-        var aDOMClass = "aCssClass";
 
-        aButton.addClass(aDOMClass);
-        var DOMButton = document.getElementById(aButton.getUniqueID());
-        
-        expect(DOMButton.tagName).toEqual("A");
-        expect(DOMButton.innerHTML).toMatch(buttonText);
+            var aComponent = createDummyComponent();
+            aComponent.getButtonText = jasmine.createSpy('getButtonText').andReturn(buttonText);
+            var aRenderer = new CUORE.Renderers.Button();
+            aRenderer.setContainer(container.id);
 
-        var innerSpan = DOMButton.getElementsByTagName('span')[0];
-        expect(innerSpan).toBeDefined();
-        expect(innerSpan.innerHTML).toEqual(buttonText);
+            aRenderer.draw(aComponent);
 
-        expect(CUORE.Dom.hasClass(DOMButton, aDOMClass)).toBeTruthy();
+
+
+            var DOMButton = document.getElementById(aRenderer.htmlID);
+
+            expect(DOMButton.tagName).toEqual("A");
+
+            var innerSpan = DOMButton.getElementsByTagName('span')[0];
+            expect(innerSpan).toBeDefined();
+            expect(innerSpan.innerHTML).toEqual(buttonText);
+        });
+
+        it("has CSS class 'button'", function() {
+            var container = createTestContainer();
+            var aButton = createDummyComponent();
+            var aRenderer = new CUORE.Renderers.Button();
+            aRenderer.setContainer(container.id);
+
+            aRenderer.draw(aButton);
+
+            var DOMButton = document.getElementById(aRenderer.htmlID);
+            expect(CUORE.Dom.hasClass(DOMButton, 'button')).toBeTruthy();
+        });
+
+
+        it("has a click event that when it is fired calls click", function() {
+            var container = createTestContainer();
+            var aButton = createDummyComponent();
+            var aRenderer = new CUORE.Renderers.Button();
+            aRenderer.setContainer(container.id);
+
+            aButton.click = jasmine.createSpy('click');
+
+            aRenderer.draw(aButton);
+
+            var DOMButton = document.getElementById(aRenderer.htmlID);
+            var events = CUORE.Dom.Event.hasEvents(DOMButton, 'click');
+
+            expect(events).toBeTruthy();
+            CUORE.Dom.Event.fire(DOMButton, 'click');
+
+            expect(aButton.click).toHaveBeenCalled();
+        });
+
+
+        it("does not click when disabled", function() {
+            var container = createTestContainer();
+            var aButton = createDummyComponent();
+            var aRenderer = new CUORE.Renderers.Button();
+            aRenderer.setContainer(container.id);
+            aButton.isEnabled = jasmine.createSpy('isEnabled').andReturn(false);
+
+            aRenderer.draw(aButton);
+
+            var DOMButton = document.getElementById(aRenderer.htmlID);
+            expect(CUORE.Dom.Event.hasEvents(DOMButton, 'click')).toBeFalsy();
+        });
+
+        createDummyComponent = function() {
+            var aComponent = {};
+            aComponent.doYouReplace = jasmine.createSpy('doYouReplace').andReturn(false);
+            aComponent.doYouHijack = jasmine.createSpy('doYouHijack').andReturn(false);
+            aComponent.getName = jasmine.createSpy('getName').andReturn('componentName');
+            aComponent.isEnabled = jasmine.createSpy('isEnabled').andReturn(true);
+
+            aComponent.getButtonName = jasmine.createSpy('getButtonName').andReturn('buttonName');
+            aComponent.getButtonText = jasmine.createSpy('getButtonText').andReturn('buttonText');
+
+            return aComponent;
+        }
+
     });
 
-    it("has CSS class 'button'", function () {
-        var container = createTestContainer();
-        var buttonClass="button";
-        var aButton = new CUORE.Components.Button("buttonName", "CanonicalKey");
-        aButton.setContainer(container.id);
-        aButton.draw();
 
-        var DOMButton = document.getElementById(aButton.getUniqueID());
-        expect(CUORE.Dom.hasClass(DOMButton, buttonClass)).toBeTruthy();
-    });
-    
-    it("has a click event that when it is fired calls click", function () {
-        var container = createTestContainer();
 
-        var buttonName = "buttonName";
-        var aButton = new CUORE.Components.Button(buttonName, "CanonicalKey");
-        aButton.setContainer(container.id);
-        spyOn(aButton,'click');
-        
-        aButton.draw();
-
-        var DOMButton = document.getElementById(aButton.getUniqueID());
-        var events = CUORE.Dom.Event.hasEvents(DOMButton, 'click');
-
-        expect(events).toBeTruthy();
-        CUORE.Dom.Event.fire(DOMButton, 'click');
-
-        expect(aButton.click).toHaveBeenCalled();
-    });
-
-    it("click calls button service", function () {
+    it("click calls button service", function() {
         var buttonName = "buttonName";
         var aButton = new CUORE.Components.Button(buttonName, "CanonicalKey");
         var someData = "Some Data";
@@ -126,8 +154,8 @@ describe("Button", function () {
 
         expect(aDirectory.execute).toHaveBeenCalledWith("BUTTON", buttonName, someData);
     });
-    
-    it("default event is stopped even when disabled", function () {
+
+    it("default event is stopped even when disabled", function() {
         var container = createTestContainer();
 
         var buttonName = "buttonName";
@@ -139,8 +167,8 @@ describe("Button", function () {
 
         expect(CUORE.Dom.Event.stopDefault).toHaveBeenCalled();
     });
-    
-    it("allows disabling and enabling behaviour", function () {
+
+    it("allows disabling and enabling behaviour", function() {
         var buttonName = "buttonName";
         var undrawnButton = new CUORE.Components.Button(buttonName, "CanonicalKey");
 
@@ -151,102 +179,40 @@ describe("Button", function () {
         expect(undrawnButton.isEnabled()).toBeTruthy();
     });
 
-    it("has enabled buttons that fire click event and disabled buttons that dont", function () {
-        var container = createTestContainer();
-        var buttonName = "buttonName";
-        var aButton = new CUORE.Components.Button(buttonName, "CanonicalKey");
-
-        var buttonClicked = false;
-
-        aButton.click = function () {
-            buttonClicked = true;
-        };
-
-        aButton.setContainer(container.id);
-
-        aButton.draw();
-
-        var DOMButton = document.getElementById(aButton.getUniqueID());
-
-        aButton.disable();
-
-        CUORE.Dom.Event.fire(DOMButton, 'click');
-        expect(buttonClicked).toBeFalsy();
-
-        buttonClicked = false;
-
-        aButton.enable();
-        CUORE.Dom.Event.fire(DOMButton, 'click');
-        expect(buttonClicked).toBeTruthy();
-    });
-
-    it("has disabled buttons that have a CSS class", function () {
-        var container = createTestContainer();
-        var buttonName = "buttonName";
-        var aButton = new CUORE.Components.Button(buttonName, "CanonicalKey");
-        aButton.setContainer(container.id);
-        aButton.disable();
-        aButton.draw();
-
-        var DOMButton = document.getElementById(aButton.getUniqueID());
-        expect(DOMButton.className.indexOf("disabled") > -1).toBeTruthy();
-
-        aButton.enable();
-
-        expect(DOMButton.className.indexOf("disabled") > -1).toBeFalsy();
-        aButton.disable();
-        expect(CUORE.Dom.hasClass(DOMButton, "disabled")).toBeTruthy();
-    });
-
-    it("removes the button from the DOM when destroy method is called", function () {
-        var container = createTestContainer();
-        var buttonName = "buttonName";
-        var aButton = new CUORE.Components.Button(buttonName, "CanonicalKey");
-        aButton.setContainer(container.id);
-
-        aButton.draw();
-        var DOMButton =  document.getElementById(aButton.getUniqueID());
-        expect(DOMButton).toBeTruthy();
-
-        aButton.destroy();
-        var DOMButton =  document.getElementById(aButton.getUniqueID());
-        expect(DOMButton).toBeFalsy();
-    });
-    
     it("uses the container as panel when component has hijack behaviour", function() {
         var container = document.createElement('a');
         container.id = "testingContainer";
         var panel = document.getElementById("xhtmlToTest");
         panel.appendChild(container);
-	
-	var aButton = new CUORE.Components.Button('buttonName', "CanonicalKey");
+
+        var aButton = new CUORE.Components.Button('buttonName', "CanonicalKey");
         aButton.behave(CUORE.Behaviours.HIJACK);
-	aButton.setContainer(container.id);
+        aButton.setContainer(container.id);
         aButton.draw();
-        
+
         expect(container.childNodes[0].tagName).toEqual('SPAN');
     });
-    
+
     it("only hijacks when panel it's an 'a' defaulting to append", function() {
         var container = document.createElement('div');
         container.id = "testingContainer";
         var panel = document.getElementById("xhtmlToTest");
         panel.appendChild(container);
-	
-	var aButton = new CUORE.Components.Button('buttonName', "CanonicalKey");
+
+        var aButton = new CUORE.Components.Button('buttonName', "CanonicalKey");
         aButton.behave(CUORE.Behaviours.HIJACK);
-	aButton.setContainer(container.id);
+        aButton.setContainer(container.id);
         aButton.draw();
-        
+
         expect(container.childNodes[0].tagName).toEqual('A');
     });
-    
-    var createTestContainer = function() {
-        var container = document.createElement('div');
-        container.id = "testingContainer";
-        var panel = document.getElementById("xhtmlToTest");
-        panel.appendChild(container);
 
-        return container;
-    };
+    var createTestContainer = function() {
+            var container = document.createElement('div');
+            container.id = "testingContainer";
+            var panel = document.getElementById("xhtmlToTest");
+            panel.appendChild(container);
+
+            return container;
+        };
 });
