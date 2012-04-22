033aa6f119a9751f3ab7e7236755a998c0b19ed5
Miguel Angel Fernandez <elmendalerenda@gmail.com>
2012-04-22 16:19:36 +0200

    removing getUniqueID from SwitchButtonSpec

--- src/Renderers/Cuore.Renderers.SwitchButton.js
+++ src/Renderers/Cuore.Renderers.SwitchButton.js
@@ -1,28 +1,22 @@
 CUORE.Renderers.SwitchButton = CUORE.Class(CUORE.Renderers.Button, {
 
     putText: function(component) {
-        var element = this.span;
-        var status  = (component.isActive()) ? 'active' : 'inactive';
+        var text = component.getActiveLabel();
+        if (!component.isActive()) {
+            text = component.getInactiveLabel();
+        }
 
-        this._putComponentText(component, status, element);
+        this.span.innerHTML = text;
     },
-    
-    setClassCSS: function (component) {
+
+    setClassCSS: function(component) {
         CUORE.Renderers.SwitchButton.parent.setClassCSS.call(this, component);
-        
+
         if (!this.panel) return;
         var newClass = (component.isActive()) ? 'on' : 'off';
         var oldClass = (component.isActive()) ? 'off' : 'on';
 
         this.addClass(newClass);
         this.removeClass(oldClass);
-     },
-
-     _putComponentText: function(component, status, element) {
-         var label = component[status + 'Label'];
-         var key   = component[status + 'Key'];
-         var text  = label || key;
-
-         element.innerHTML = text;
-     }
+    }
 });
