d5f57438a86d7b3ff7fab8e8af02f68e218b249f
Miguel Angel Fernandez <elmendalerenda@gmail.com>
2012-04-23 09:31:54 +0200

    LabelPanel displays the text and showcase partially fixed

---
+++ src/Renderers/Cuore.Renderers.LabelPanel.js
@@ -0,0 +1,12 @@
+CUORE.Renderers.LabelPanel = CUORE.Class(CUORE.Renderer, {
+
+
+    updateWhenDrawn: function(component) {
+        CUORE.Renderers.LabelPanel.parent.updateWhenDrawn.call(this, component);
+
+        this.panel.innerHTML = component.getLabelText();
+    },
+
+
+
+});
