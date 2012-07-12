5f8304eb61329b0e0fcaffbd9604106e9032c0a3
Miguel Angel <mike@becodemyfriend.com>
2012-07-12 13:55:11 +0200

    making event system compatible with IE8 and IE9

--- src/Cuore.Dom.Event.js
+++ src/Cuore.Dom.Event.js
@@ -3,19 +3,19 @@ CUORE.Dom.Event = (function(undefined) {
     var _eventManager = {
         add: function(element, eventName, functionToExecute) {
             this._initializeStructure(element, eventName);
-            
+
             element.events[eventName].push(functionToExecute);
         },
 
         remove: function(element, eventName) {
-            if(this.hasEvents(element, eventName)) {
+            if (this.hasEvents(element, eventName)) {
                 element.events[eventName] = [];
             }
         },
 
         fire: function(element, eventName) {
             if (!this.hasEvents(element, eventName)) return;
-            
+
             var callbacks = element.events[eventName];
             for (var i = 0, len = callbacks.length; i < len; i++) {
                 callbacks[i]();
@@ -23,101 +23,115 @@ CUORE.Dom.Event = (function(undefined) {
         },
 
         hasEvents: function(element, eventName) {
-            var elementHasEvents = (element.events && element.events[eventName]
-                                    && element.events[eventName].length > 0);
+            var elementHasEvents = (element.events && element.events[eventName] && element.events[eventName].length > 0);
 
             return (elementHasEvents) ? true : false;
         },
-        
-        _initializeStructure: function(element, eventName){
-            !element.events && (element.events = {});
-            !element.events[eventName] && (element.events[eventName] = []);
+
+        _initializeStructure: function(element, eventName) {
+            if (!element.events) {
+                element.events = {};
+            }
+            if (!element.events[eventName]) {
+                element.events[eventName] = [];
+            }
         }
     };
-    
+
     var add = function(el, type, fn) {
-        _eventManager.add(el, type, fn);
-        _addEventListener(el, type, fn);
-    };
+            _eventManager.add(el, type, fn);
+            _addEventListener(el, type, fn);
+        };
 
     var remove = function(el, type, fn) {
-        _removeEventListener(el, type, fn);
-        _eventManager.remove(el, type);
-    };
-    
-    var stopDefault = function(el, type) {
-        add(el, type, function(event) {
-            if (!event) return;
+            _removeEventListener(el, type, fn);
+            _eventManager.remove(el, type);
+        };
 
-            event.preventDefault();
-            event.stopPropagation();
-        });
-    };
+    var stopDefault = function(el, type) {
+            add(el, type, function(event) {
+                if (!event) return;
+
+                if (_hasIE8EventSystem()) {
+                    event.returnValue = false;
+                    event.cancelBubble = true;
+                } else {
+                    event.preventDefault();
+                    event.stopPropagation();
+                }
+            });
+        };
 
     var hasEvents = function(el, type) {
-        return _eventManager.hasEvents(el, type);
-    };
+            return _eventManager.hasEvents(el, type);
+        };
 
     var fire = function(el, type) {
-        _eventManager.fire(el, type);
-    };
+            _eventManager.fire(el, type);
+        };
 
-    var _addEventListener = function(el, type, fn){
-        
-        if (_isW3cListener()) { 
-            el.addEventListener(type, fn, false);
-        }
-        
-        if (_isIEListener()) { 
-            el.attachEvent('on' + type, fn);
-        }
-        
-        if (!_isIEListener() && !_isW3cListener()) { 
-            el['on' + type] = fn;
-        }
-    };
-   
-    var _removeEventListener = function(el, type, fn){
-        
-        if (_isW3cListener()) { 
-            _removeMultipleEventListener(el, type);
-        }
-        
-        if (_isIEListener()) { 
-            _removeMultipleDetachEvent(el, type);
-        }
-        
-        if (!_isIEListener() && !_isW3cListener()) { 
-            el['on' + type] = null;
-        }
-    };
-    
-    var _isW3cListener = function(){
-        return (typeof window.addEventListener === 'function');
-    };
-    
-    var _isIEListener = function(){
-        return (typeof document.attachEvent === 'function');
-    };
-    
-    var _removeMultipleEventListener = function(element, eventName){
-        if (!_eventManager.hasEvents(element, eventName)) return;
-        
-        var callbacks = element.events[eventName];
-        for (var i = 0, len = callbacks.length; i < len; i++) {
-            element.removeEventListener(eventName, callbacks[i], false);
-        }
-    };
-    
-    var _removeMultipleDetachEvent = function(element, eventName){
-        if (!_eventManager.hasEvents(element, eventName)) return;
-        
-        var callbacks = element.events[eventName];
-        for (var i = 0, len = callbacks.length; i < len; i++) {
-            element.detachEvent('on' + eventName, callbacks[i]);
-        }
-    };
-    
+    var _hasIE8EventSystem = function() {
+            return (!event.preventDefault && !event.stopPropagation);
+        };
+
+    var _addEventListener = function(el, type, fn) {
+
+            if (_isW3cListener()) {
+                el.addEventListener(type, fn, false);
+            }
+
+            if (_isIEListener()) {
+                el.attachEvent('on' + type, fn);
+            }
+
+            if (!_isIEListener() && !_isW3cListener()) {
+                el['on' + type] = fn;
+            }
+        };
+
+    var _removeEventListener = function(el, type, fn) {
+
+            if (_isW3cListener()) {
+                _removeMultipleEventListener(el, type);
+            }
+
+            if (_isIEListener()) {
+                _removeMultipleDetachEvent(el, type);
+            }
+
+            if (!_isIEListener() && !_isW3cListener()) {
+                el['on' + type] = null;
+            }
+        };
+
+    var _isW3cListener = function() {
+            return (typeof window.addEventListener === 'function');
+        };
+
+    var _isIEListener = function() {
+            if(_isW3cListener()) return false;
+            
+            return (typeof document.attachEvent === 'function' || typeof document.attachEvent === 'object');
+        };
+
+    var _removeMultipleEventListener = function(element, eventName) {
+            if (!_eventManager.hasEvents(element, eventName)) return;
+
+            var callbacks = element.events[eventName];
+            for (var i = 0, len = callbacks.length; i < len; i++) {
+                element.removeEventListener(eventName, callbacks[i], false);
+            }
+        };
+
+    var _removeMultipleDetachEvent = function(element, eventName) {
+            if (!_eventManager.hasEvents(element, eventName)) return;
+
+            var callbacks = element.events[eventName];
+            for (var i = 0, len = callbacks.length; i < len; i++) {
+                element.detachEvent('on' + eventName, callbacks[i]);
+            }
+        };
+
     return {
         add: add,
         remove: remove,
