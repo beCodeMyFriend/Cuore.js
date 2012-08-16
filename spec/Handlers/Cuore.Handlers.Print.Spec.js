1f0a5b9adcdd58c13ca8809d7d3e527b4bd36b32
chapuzzo <luismax@gmail.com>
2012-08-16 14:03:25 +0200

    Updatind Cuore.Handlers.Print.Spec.js to use jasmine spies and Cuore matchers

--- spec/Handlers/Cuore.Handlers.Print.Spec.js
+++ spec/Handlers/Cuore.Handlers.Print.Spec.js
@@ -1,26 +1,22 @@
 describe("PrintHandler", function () {
 	
-    it("inherits Handlet", function () {
-        var theHandler = new CUORE.Handlers.Print();
-
-        expect(theHandler instanceof CUORE.Handlers.Print).toBeTruthy();
-        expect(theHandler instanceof CUORE.Handler).toBeTruthy();
+    beforeEach(function() {
+            this.addMatchers({
+                toBeInstanceOf: CUORE.Matchers.toBeInstanceOf
+            });
     });
 
-    it("executes window.print", function () {
-        var originalPrint = window.print;
 
-        var printCalled = false;
-        window.print = function () {
-            printCalled = true;
-        };
-
+    it("inherits Handlet", function () {
         var theHandler = new CUORE.Handlers.Print();
+        expect(theHandler).toBeInstanceOf(CUORE.Handlers.Print);
+        expect(theHandler).toBeInstanceOf(CUORE.Handler);
+    });
 
+    it("executes window.print", function () {        
+        window.print = jasmine.createSpy("print");
+        var theHandler = new CUORE.Handlers.Print();
         theHandler.handle();
-
-        expect(printCalled).toBeTruthy();
-
-        window.print = originalPrint;
+        expect(window.print).toHaveBeenCalled();
     });
 });
