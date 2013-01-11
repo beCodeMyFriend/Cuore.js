cb0451d7b24a12430a137c97b6bee6637f129ff7
Miguel Angel <elmendalerenda@gmail.com>
2013-01-11 10:39:08 +0100

    method for creating an item in renderer list now have access to component

--- spec/Renderers/Cuore.Renderers.List.Spec.js
+++ spec/Renderers/Cuore.Renderers.List.Spec.js
@@ -1,106 +1,106 @@
 describe('List Renderer', function() {
-       
-    afterEach(function(){
-        var container = document.getElementById('xhtmlToTest');
-        container.innerHTML = '';
-    });
-   
-    beforeEach(function() {
 
-	this.addMatchers({
-	    toContainAnElement: function(HTMLElementType) {
-		var figures = this.actual.getElementsByTagName(HTMLElementType);
-		return (figures.length > 0);
-	    },
-	    
-	    toContainClass: function(classname) {
-		return CUORE.Dom.hasClass(this.actual,classname);
-	    },
-	    
-	    toContainAsFirstElement: function(HTMLElementType) {
-		var tagName=this.actual.childNodes[0].tagName;
-		var hasElement =(tagName.toLowerCase() === HTMLElementType.toLowerCase())
-		return hasElement;
-	    }
-	})
+  afterEach(function() {
+    var container = document.getElementById('xhtmlToTest');
+    container.innerHTML = '';
+  });
 
-    });
+  beforeEach(function() {
 
-    it("inherits Renderer", function() {
-        var aRenderer = new CUORE.Renderers.List();
-        expect(aRenderer instanceof CUORE.Renderers.List).toBeTruthy();
-        expect(aRenderer instanceof CUORE.Renderer).toBeTruthy();
-    });
+    this.addMatchers({
+      toContainAnElement: function(HTMLElementType) {
+        var figures = this.actual.getElementsByTagName(HTMLElementType);
+        return(figures.length > 0);
+      },
 
-    it("renderizes an unordered list ", function() {
-        var container = createTestContainer();
-        var aComponent = getDummyComponent();
+      toContainClass: function(classname) {
+        return CUORE.Dom.hasClass(this.actual, classname);
+      },
 
-        var aRenderer = new CUORE.Renderers.List();
-        aRenderer.setContainer(container.id);
+      toContainAsFirstElement: function(HTMLElementType) {
+        var tagName = this.actual.childNodes[0].tagName;
+        var hasElement = (tagName.toLowerCase() === HTMLElementType.toLowerCase())
+        return hasElement;
+      }
+    })
 
-        aRenderer.render(aComponent);
+  });
 
-        var DOMElement = document.getElementById(container.id);
-        expect(DOMElement).toContainAsFirstElement('ul');
-        var innerElement = DOMElement.childNodes[0];
-    });
-    
-    it("renderizes an li by item", function () {
-        var container = createTestContainer();
-	var aComponent = getDummyComponent();
-	
-	var aRenderer = new CUORE.Renderers.List();
-	aRenderer.setContainer(container.id);
+  it("inherits Renderer", function() {
+    var aRenderer = new CUORE.Renderers.List();
+    expect(aRenderer instanceof CUORE.Renderers.List).toBeTruthy();
+    expect(aRenderer instanceof CUORE.Renderer).toBeTruthy();
+  });
 
-	aRenderer.render(aComponent);
-	
-        var DOMElement = document.getElementById(container.id);
-	var innerElement = DOMElement.childNodes[0];
-        expect(innerElement.childNodes.length).toEqual(3);
-	expect(innerElement.getElementsByTagName('li').length).toEqual(3);
-    });
+  it("renderizes an unordered list ", function() {
+    var container = createTestContainer();
+    var aComponent = getDummyComponent();
 
-    it("renderizes every li with right content", function () {
-        var container = createTestContainer();
-	var aComponent = getDummyComponent();
-	
-	var aRenderer = new CUORE.Renderers.List();
-	aRenderer.setContainer(container.id);
+    var aRenderer = new CUORE.Renderers.List();
+    aRenderer.setContainer(container.id);
 
-	aRenderer.render(aComponent);
-	
-        var DOMElement = document.getElementById(container.id);
-	var innerElement = DOMElement.childNodes[0].childNodes[0];
-        
-        expect(innerElement.innerHTML).toEqual('item');
-    });
+    aRenderer.render(aComponent);
 
-    var createTestContainer = function() {
-       var container = document.createElement('div');
-       container.id = "testingContainer";
-       var panel = document.getElementById("xhtmlToTest");
-       panel.appendChild(container);
+    var DOMElement = document.getElementById(container.id);
+    expect(DOMElement).toContainAsFirstElement('ul');
+    var innerElement = DOMElement.childNodes[0];
+  });
 
-       return container;
+  it("renderizes an li by item", function() {
+    var container = createTestContainer();
+    var aComponent = getDummyComponent();
+
+    var aRenderer = new CUORE.Renderers.List();
+    aRenderer.setContainer(container.id);
+
+    aRenderer.render(aComponent);
+
+    var DOMElement = document.getElementById(container.id);
+    var innerElement = DOMElement.childNodes[0];
+    expect(innerElement.childNodes.length).toEqual(3);
+    expect(innerElement.getElementsByTagName('li').length).toEqual(3);
+  });
+
+  it("renderizes every li with right content", function() {
+    var container = createTestContainer();
+    var aComponent = getDummyComponent();
+
+    var aRenderer = new CUORE.Renderers.List();
+    aRenderer.setContainer(container.id);
+
+    aRenderer.render(aComponent);
+
+    var DOMElement = document.getElementById(container.id);
+    var innerElement = DOMElement.childNodes[0].childNodes[0];
+
+    expect(innerElement.innerHTML).toEqual('item');
+  });
+
+  var createTestContainer = function() {
+      var container = document.createElement('div');
+      container.id = "testingContainer";
+      var panel = document.getElementById("xhtmlToTest");
+      panel.appendChild(container);
+
+      return container;
     };
 
-    var getDummyComponent = function() {
-       var aComponent = {};
+  var getDummyComponent = function() {
+      var aComponent = {};
 
-       aComponent.isEnabled = jasmine.createSpy().andReturn(true);
-       aComponent.getText = jasmine.createSpy().andReturn('anyText');
-       aComponent.getName = jasmine.createSpy().andReturn('anyName');
-       aComponent.doYouReplace = jasmine.createSpy().andReturn(true);
-       aComponent.doYouHijack = jasmine.createSpy().andReturn(false);
-       aComponent.getContainer = jasmine.createSpy().andReturn("testingContainer");
+      aComponent.isEnabled = jasmine.createSpy().andReturn(true);
+      aComponent.getText = jasmine.createSpy().andReturn('anyText');
+      aComponent.getName = jasmine.createSpy().andReturn('anyName');
+      aComponent.doYouReplace = jasmine.createSpy().andReturn(true);
+      aComponent.doYouHijack = jasmine.createSpy().andReturn(false);
+      aComponent.getContainer = jasmine.createSpy().andReturn("testingContainer");
 
-        var anItem = "item";
+      var anItem = "item";
 
-       aComponent.item =jasmine.createSpy().andReturn(anItem);
-       aComponent.size = jasmine.createSpy().andReturn(3);
-       return aComponent;
-   };
+      aComponent.item = jasmine.createSpy().andReturn(anItem);
+      aComponent.size = jasmine.createSpy().andReturn(3);
+      return aComponent;
+    };
 
 
 
