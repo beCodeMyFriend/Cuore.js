796a35802b1315b9909fe977e635f1ea37c2df9a
XaV1Uzz <xavi@becodemyfriend.com>
2013-04-22 19:15:40 +0200

    class system under test

---
+++ spec/Cuore.Class.Spec.js
@@ -0,0 +1,42 @@
+describe("Core.Class", function() {
+
+    var parentClass,childClass;
+
+    beforeEach(function() {
+        parentClass = CUORE.Class(null, {
+            aProperty: 'aProperty',
+            init: function(){},
+            aMethod: function(){},
+            anotherMethod: function(){var foo=0;}
+        });
+
+        childClass = CUORE.Class(parentClass, {
+            init: jasmine.createSpy(),
+            anotherMethod: function(){var bar=0;}
+        });
+    });
+
+    it(" childClass inherits properties and methods", function() {
+        var instance = new childClass();
+        expect(instance.aProperty).toEqual('aProperty');
+        expect(typeof instance.aMethod).toEqual('function'); 
+    });
+
+    it(" childClass inherits method implementation", function() {
+        var parent= new parentClass();
+        var instance = new childClass();
+        expect(instance.aMethod).toBe(parent.aMethod);
+    });
+
+    it(" childClass can override method implementation", function() {
+        var parent= new parentClass();
+        var instance = new childClass();
+        expect(instance.anotherMethod).not.toBe(parent.anotherMethod);
+    });
+
+    it(" uses init at instanciation", function() {
+        var instance = new childClass();
+        expect(instance.init).toHaveBeenCalled();
+    });
+
+});
